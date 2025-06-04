031e4b01e10b33a45ef191c6a94bb7d873dd43d1
Israel Teixeira <israeltx@gmail.com>
2025-06-04 11:40:18 -0300

    Get currency input value

--- scripts.js
+++ scripts.js
@@ -1,8 +1,16 @@
+// Get form elements
+const form = document.querySelector('form');
 const amount = document.getElementById('amount');
+const currency = document.getElementById('currency');
 
 // Get only numbers from the amount input
 amount.addEventListener('input', () => {
-  const hasCharacterRegex = /\D+/g
-  amount.value = amount.value.replace(hasCharacterRegex, '')
-  console.log(amount.value)
-})
+  const hasCharacterRegex = /\D+/g;
+  amount.value = amount.value.replace(hasCharacterRegex, '');
+});
+
+// Lister the submit event from the form
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  console.log(currency.value)
+});
