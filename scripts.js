0c9bd2405e0d5886f7ee8c61d15c874629ed985b
Israel Teixeira <israeltx@gmail.com>
2025-06-04 11:31:59 -0300

    Add validation rules for the input

--- scripts.js
+++ scripts.js
@@ -2,5 +2,7 @@ const amount = document.getElementById('amount');
 
 // Get only numbers from the amount input
 amount.addEventListener('input', () => {
+  const hasCharacterRegex = /\D+/g
+  amount.value = amount.value.replace(hasCharacterRegex, '')
   console.log(amount.value)
 })
