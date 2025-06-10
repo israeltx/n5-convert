c147fae66ed0c0668c186c0c3a98f7a8371a8e1f
Israel Teixeira <israeltx@gmail.com>
2025-06-10 15:42:18 -0300

    Format the currency on the amount field

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 // Select form elements
 const amount = document.getElementById('amount')
 
+// Capture input event for formatting
 amount.oninput = () => {
+  // Remove non numerical characters from the input value
   let value = amount.value.replace(/\D/g, '')
-  amount.value = value
+
+  // Convert values to cents (150/100 = 1.5 or R$ 1,50)
+  value = Number(value) / 100
+
+  // Update the value
+  amount.value = formatCurrencyBRL(value)
+}
+
+function formatCurrencyBRL(value) {
+    // Format the value to Brazilian Reais
+    value = value.toLocaleString('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    })
+    // Return the formatted value
+    return value
 }
