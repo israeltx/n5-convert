79604901330c3613b5bb278344a5f5c1e6a80a1f
Israel Teixeira <israeltx@gmail.com>
2025-06-11 12:20:59 -0300

    Obtain data from the form

--- script.js
+++ script.js
@@ -26,6 +26,18 @@ function formatCurrencyBRL(value) {
     return value
 }
 
+// Obtain form values through the submit event
 form.onsubmit = (event) => {
+  // Prevent the page from refreshing
   event.preventDefault()
+
+  // Object with new expense details
+  const newExpense = {
+    id: new Date().getTime(),
+    expense: expense.value,
+    category_id: category.value,
+    category_name: category.options[category.selectedIndex].text,
+    amount: amount.value,
+    created_at: new Date(),
+  }
 }
